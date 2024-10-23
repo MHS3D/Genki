0f50b96d5e7ae3510b84c18938ae2442102335b2
AnnsAnn <git@annsann.eu>
2024-10-23 17:08:00 +0200

    Add support for both sensors

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{thread::sleep, time::Duration};
 use anyhow::{bail, Result};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::{delay::Delay, i2c::{I2c, I2cConfig, I2cDriver}, prelude::Peripherals}};
 
+use max3010x::{Led, Max3010x, SampleAveraging};
 use mpu6050::*;
 
 mod wifi;
@@ -37,12 +38,32 @@ fn main() -> Result<()> {
     let rx = peripherals.pins.gpio3;
     let tx = peripherals.pins.gpio4;
 
+    // Init I2C
     let i2c_config = I2cConfig::new().baudrate(esp_idf_svc::hal::prelude::Hertz(BAUDRATE));
     let mut i2c_driver = I2cDriver::new(i2c, sda, scl, &i2c_config)?;
-    let mut mpu = Mpu6050::new(i2c_driver);
 
-    let mut delay = Delay::new_default();
-    let _ = mpu.init(&mut delay);
+    // Init MPU6050 Gyro
+    // Only one I2C device can be initialized at a time, @TODO: proper mutex handling to have both at the same time
+    // let mut mpu = Mpu6050::new(i2c_driver);
+    // let mut delay = Delay::new_default();
+    // let _ = mpu.init(&mut delay);
+
+    // Init MAX3010 Pulse Oximeter
+    let mut max3010 = Max3010x::new_max30102(i2c_driver);
+    max3010.set_sample_averaging(SampleAveraging::Sa4).unwrap();
+    max3010.set_pulse_amplitude(Led::All, 15).unwrap();
+    max3010.enable_fifo_rollover().unwrap();
+
+    // Change to heart rate mode
+    let mut heartrate = max3010.into_heart_rate().unwrap();
+    let mut data: [u32; 4] = [0; 4];
+    let samples_read = heartrate.read_fifo(&mut data).unwrap();
+    log::info!("Temperature: {:?} | Read Samples: {}", data, samples_read);
+
+    // Change to oximeter mode
+    let mut oximeter = heartrate.into_oximeter().unwrap();
+    let samples_read = oximeter.read_fifo(&mut data).unwrap();
+    log::info!("Oximeter: {:?} | Read Samples: {}", data, samples_read);
 
     let sysloop = EspSystemEventLoop::take()?;
 

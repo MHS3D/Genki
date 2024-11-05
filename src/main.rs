3676db78c810eb961dbe96a3249f2889b31274c1
AnnsAnn <git@annsann.eu>
2024-11-05 15:35:13 +0100

    Try to get both sensors working

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use esp_idf_svc::{
     hal::{
         delay::Delay,
         i2c::{I2c, I2cConfig, I2cDriver},
+        peripheral,
         prelude::Peripherals,
     },
     mqtt::client::*,
@@ -24,35 +25,43 @@ const BAUDRATE: u32 = 100;
 /// file `cfg.toml`.
 #[toml_cfg::toml_config]
 pub struct Config {
-    #[default("baguette")]
+    #[default("genkiwlan")]
     wifi_ssid: &'static str,
-    #[default("broetchen123")]
+    #[default("genkipassword")]
     wifi_psk: &'static str,
-    #[default("mqtt://hivemq.cloud:8883")]
+    #[default("mqtt://68a4e2c14c1149f5b8b036ee063aa8c6.s1.eu.hivemq.cloud:8883")]
     mqtt_url: &'static str,
-    #[default("baguette")]
+    #[default("GENKIUHR")]
     mqtt_client_id: &'static str,
     #[default("")]
     mqtt_topic: &'static str,
-    #[default("baguette")]
+    #[default("GENKIUHRUSER")]
     mqtt_username: &'static str,
-    #[default("broetchen123")]
+    #[default("5xmrCzXy3ak8I2")]
     mqtt_password: &'static str,
 }
 
-fn mqtt_create(
-    url: &str,
-    client_id: &str,
-) -> Result<(EspMqttClient<'static>, EspMqttConnection), EspError> {
+fn mqtt_create() -> Result<(EspMqttClient<'static>, EspMqttConnection), EspError> {
+    log::info!("Creating MQTT connection to {}", CONFIG.mqtt_url);
+
     let (mqtt_client, mqtt_conn) = EspMqttClient::new(
-        url,
+        &CONFIG.mqtt_url,
         &MqttClientConfiguration {
-            client_id: Some(client_id),
+            client_id: Some(CONFIG.mqtt_username),
+            username: Some(CONFIG.mqtt_username),
+            password: Some(CONFIG.mqtt_password),
             ..Default::default()
         },
     )?;
 
+    log::info!("MQTT connection created");
+
     Ok((mqtt_client, mqtt_conn))
+}
+
+enum SensorToUse {
+    MAX3010,
+    MPU6050,
 }
 
 fn main() -> Result<()> {
@@ -65,42 +74,16 @@ fn main() -> Result<()> {
 
     log::info!("Hello, world!");
 
-    let peripherals = Peripherals::take().unwrap();
-    let i2c = peripherals.i2c0;
-    let sda = peripherals.pins.gpio22;
-    let scl = peripherals.pins.gpio23;
-    let rx = peripherals.pins.gpio17;
-    let tx = peripherals.pins.gpio16;
+    // let rx = peripherals.pins.gpio17;
+    // let tx = peripherals.pins.gpio16;
 
     log::info!("Defined peripherals");
-
-    // Init I2C
-    let i2c_config = I2cConfig::new().baudrate(esp_idf_svc::hal::prelude::Hertz(BAUDRATE));
-    log::info!("I2C Config: {:?}", i2c_config);
-
-    let i2c_driver = I2cDriver::new(i2c, sda, scl, &i2c_config)?;
-    log::info!("I2C Driver init");
-
-    // Init MPU6050 Gyro
-    // Only one I2C device can be initialized at a time, @TODO: proper mutex handling to have both at the same time
-    let mut mpu = Mpu6050::new(i2c_driver);
-    let mut delay = Delay::new_default();
-    let _ = mpu.init(&mut delay);
-    log::info!("MPU6050 init");
-
-    // Init MAX3010 Pulse Oximeter
-    // let mut max3010 = Max3010x::new_max30102(i2c_driver);
-    // log::info!("MAX3010 init");
-
-    // max3010.set_sample_averaging(SampleAveraging::Sa4).unwrap();
-    // max3010.set_pulse_amplitude(Led::All, 15).unwrap();
-    // max3010.enable_fifo_rollover().unwrap();
-    // log::info!("MAX3010 config");
 
     let sysloop = EspSystemEventLoop::take()?;
 
     let app_config = CONFIG;
     // Connect to the Wi-Fi network
+    let peripherals = Peripherals::take().unwrap();
     let _wifi = match wifi::connect_wifi(
         app_config.wifi_ssid,
         app_config.wifi_psk,
@@ -117,90 +100,116 @@ fn main() -> Result<()> {
         }
     };
 
-    let (mut client, mut conn) = mqtt_create(&app_config.mqtt_url, &app_config.mqtt_client_id)?;
-
-    log::info!("MQTT Listening for messages");
-
-    while let Ok(event) = conn.next() {
-        log::info!("[Queue] Event: {}", event.payload());
-    }
-
     log::info!("Connection closed");
 
     // Just to give a chance of our connection to get even the first published message
     std::thread::sleep(Duration::from_millis(500));
 
-    let payload = "Hello from esp-mqtt-demo!";
-
-    // server::init_server();
-
-    // Change to heart rate mode
-
-    //let mut heartrate = max3010.into_heart_rate().unwrap();
+    // server::init_server()
 
     // Infinite loop
+
+    let mut sensor_to_use = SensorToUse::MPU6050;
+
     loop {
-        client.enqueue("baguette", QoS::AtMostOnce, false, payload.as_bytes())?;
-
-        log::info!("Published \"{payload}\" to topic \"baguette\"");
-
         let sleep_secs = 2;
 
-        log::info!("Now sleeping for {sleep_secs}s...");
-        std::thread::sleep(Duration::from_secs(sleep_secs));
-        // let temperature = match max3010.read_temperature() {
-        //     Ok(temp) => temp,
-        //     Err(err) => {
-        //         log::error!("Error reading temperature: {:?}", err);
-        //         continue;
-        //     }
-        // };
+        let peripherals = Peripherals::take().unwrap();
+        let i2c = peripherals.i2c0;
+        let sda = peripherals.pins.gpio22;
+        let scl = peripherals.pins.gpio23;
 
-        log::info!("Reading Accel");
+        // Init I2C
+        let i2c_config = I2cConfig::new().baudrate(esp_idf_svc::hal::prelude::Hertz(BAUDRATE));
+        log::info!("I2C Config: {:?}", i2c_config);
 
-        let accel = match mpu.get_acc() {
-            Ok(acc) => {
-                log::info!("Received Accel: {:?}", acc);
-                acc
-            }
-            Err(err) => {
-                log::error!("Error reading accelerometer: {:?}", err);
-                continue;
-            }
-        };
+        let i2c_driver = I2cDriver::new(i2c, sda, scl, &i2c_config)?;
+        log::info!("I2C Driver init");
 
-        let accel_angles = match mpu.get_acc_angles() {
-            Ok(angles) => {
-                log::info!("Received Accel Angles: {:?}", angles);
-                angles
-            }
-            Err(err) => {
-                log::error!("Error reading accelerometer angles: {:?}", err);
-                continue;
-            }
-        };
+        match sensor_to_use {
+            SensorToUse::MAX3010 => {
+                // Init MAX3010 Pulse Oximeter
+                let mut max3010 = Max3010x::new_max30102(i2c_driver);
+                log::info!("MAX3010 init");
 
-        let gyrodata = match mpu.get_gyro() {
-            Ok(gyro) => {
-                log::info!("Received Gyro: {:?}", gyro);
-                gyro
-            }
-            Err(err) => {
-                log::error!("Error reading gyroscope: {:?}", err);
-                continue;
-            }
-        };
+                max3010.set_sample_averaging(SampleAveraging::Sa4).unwrap();
+                max3010.set_pulse_amplitude(Led::All, 15).unwrap();
+                max3010.enable_fifo_rollover().unwrap();
+                log::info!("MAX3010 config");
 
-        //log::info!("Temperature: {}", temperature);
-        // log::info!("Reading Heartrate");
-        // let mut data: [u32; 4] = [0; 4];
-        // let samples_read = heartrate.read_fifo(&mut data).unwrap();
-        // log::info!("Temperature: {:?} | Read Samples: {}", data, samples_read);
+                let temperature = max3010.read_temperature().unwrap();
 
-        // // Change to oximeter mode
-        // let mut oximeter = heartrate.into_oximeter().unwrap();
-        // let samples_read = oximeter.read_fifo(&mut data).unwrap();
-        // log::info!("Oximeter: {:?} | Read Samples: {}", data, samples_read);
+                log::info!("Temperature: {}", temperature);
+
+                log::info!("Reading Heartrate");
+                let mut heartrate = max3010.into_heart_rate().unwrap();
+                let mut data: [u32; 4] = [0; 4];
+                let samples_read = heartrate.read_fifo(&mut data).unwrap();
+                log::info!("Temperature: {:?} | Read Samples: {}", data, samples_read);
+
+                // Change to oximeter mode
+                let mut oximeter = heartrate.into_oximeter().unwrap();
+                let samples_read = oximeter.read_fifo(&mut data).unwrap();
+                log::info!("Oximeter: {:?} | Read Samples: {}", data, samples_read);
+
+                sensor_to_use = SensorToUse::MPU6050;
+            }
+            SensorToUse::MPU6050 => {
+                // Init MPU6050 Gyro
+                // Only one I2C device can be initialized at a time, @TODO: proper mutex handling to have both at the same time
+                let mut mpu = Mpu6050::new(i2c_driver);
+                let mut delay = Delay::new_default();
+                let _ = mpu.init(&mut delay);
+                log::info!("MPU6050 init");
+
+                log::info!("Now sleeping for {sleep_secs}s...");
+                std::thread::sleep(Duration::from_secs(sleep_secs));
+                // let temperature = match max3010.read_temperature() {
+                //     Ok(temp) => temp,
+                //     Err(err) => {
+                //         log::error!("Error reading temperature: {:?}", err);
+                //         continue;
+                //     }
+                // };
+
+                log::info!("Reading Accel");
+
+                let accel = match mpu.get_acc() {
+                    Ok(acc) => {
+                        log::info!("Received Accel: {:?}", acc);
+                        acc
+                    }
+                    Err(err) => {
+                        log::error!("Error reading accelerometer: {:?}", err);
+                        continue;
+                    }
+                };
+
+                let accel_angles = match mpu.get_acc_angles() {
+                    Ok(angles) => {
+                        log::info!("Received Accel Angles: {:?}", angles);
+                        angles
+                    }
+                    Err(err) => {
+                        log::error!("Error reading accelerometer angles: {:?}", err);
+                        continue;
+                    }
+                };
+
+                let gyrodata = match mpu.get_gyro() {
+                    Ok(gyro) => {
+                        log::info!("Received Gyro: {:?}", gyro);
+                        gyro
+                    }
+                    Err(err) => {
+                        log::error!("Error reading gyroscope: {:?}", err);
+                        continue;
+                    }
+                };
+
+                sensor_to_use = SensorToUse::MAX3010;
+            }
+        }
 
         sleep(Duration::from_secs(1))
     }

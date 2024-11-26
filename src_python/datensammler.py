6b41c83bf6550d6fda6931286a855e758c71557e
annsann <git@annsann.eu>
2024-11-26 10:33:07 +0100

    faster data collection

--- src_python/datensammler.py
+++ src_python/datensammler.py
@@ -12,7 +12,6 @@ while True:
         if response.status_code == 200:
             data = json.loads(response.text)
             
-            
             # Merge the new data with the existing data
             for key, value in data.items():
                 if key not in unique_responses:
@@ -25,7 +24,7 @@ while True:
     except requests.RequestException as e:
         print(f"Request failed: {e}")
     
-    time.sleep(1)  # Wait for 1 second before making the next request
+    time.sleep(0.2)  # Wait for 0.2 second before making the next request
     
     with open('data.json', 'w') as f:
         json.dump(unique_responses, f)
